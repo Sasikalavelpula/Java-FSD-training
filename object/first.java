7c3462e6a02531d2ed07b85b54e21f86dca60469
sasikalavelpula <sasikalavelpula45@gmail.com>
2026-09-23 09:45:56 +0530

    Day-1 java fsd training  programs

---
+++ object/first.java
@@ -0,0 +1,9 @@
+class car{
+    String brand="Toyota";
+}
+public class Main{
+    public static void main(String[] args){
+        car myCar=new car();
+        System.out.println(myCar.brand);
+    }
+}
